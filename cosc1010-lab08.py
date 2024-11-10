2f1f98f1d43e595b039856006755d11e23a89bd5
megs9475 <mlongua@uwyo.edu>
2024-11-10 22:44:04 +0000

    finished lab

--- cosc1010-lab08.py
+++ cosc1010-lab08.py
@@ -1,8 +1,8 @@
-# Your Name Here
+# Meghan Longua
 # UWYO COSC 1010
-# Submission Date
-# Lab XX
-# Lab Section:
+# Submission Date: 11/6/2024
+# Lab 08
+# Lab Section:15
 # Sources, people worked with, help given to:
 # your
 # comments
@@ -14,9 +14,23 @@
 # Other wise return the converted int or float 
 # Floats should only have one decimal point in them 
 
+def check_string(string):
+    if (string.isnumeric()):
+        return int(string)
+    if (string.count(".") == 1 and
+        string.replace(".", "").isdigit()):
+        return float(string)
+    if (string[0] == "-" and 
+        string.replace("-", "").isdigit()):
+        return int(string)
+    if (string[0] == "-" and
+        string.replace("-", "") and 
+        string[1:].count(".") == 1 and
+        string[1:].replace(".", "").isdigit()):
+        return float(string)
+    return False
 
 print("*" * 75)
-
 
 # Point-slope y = mx + b
 # This is used in mathematics to determine what the value y would be for any given x
@@ -32,11 +46,33 @@ print("*" * 75)
 # Check to make sure that the lower bound is less than or equal to the upper bound
 # m, b can be floats or integers
 # the bounds must be integers, if not return false
-
 # Create a while loop to prompt users for their input for the four variables
 # Exit on the word exit
 # Remember all inputs are strings, but the function needs ints or floats
 # Call your function and print the resulting list
+
+def slope_intercept(m, b, min_x, max_x):
+    slope = check_string(m)
+    y_int = check_string(b)
+    lower = check_string(min_x)
+    upper = check_string(max_x)
+    if(not isinstance(min_x, int)):
+        print("Lower bound (min_x) must be a whole number")
+    if(not isinstance(max_x, int)):
+        print("Upper bound (max_x) must be a whole number")
+    if upper < lower:
+        print("Lower bound must be equal to or less than upper bound")
+    return False
+
+while True:
+    values = input("Enter values for m, b, min_x, and max_x for formula y = mx + b. Enter 'exit' to exit")
+    inputs = values.split(",")
+    if (len(inputs) != 4):
+        print("Invalid format")
+    if (values == 'exit'):
+        break
+for x in range(lower, upper + 1):
+    y = slope * x + y_int
 
 print("*" * 75)
 
@@ -48,3 +84,27 @@ print("*" * 75)
 # Create a loop like above to prompt the user for input for the three values
 # Create a second function that just does the square root operation 
     # If the number you are trying to take the square root of is negative, return null
+
+def quadratic_coefficients(A, B, C):
+    a = check_string(A)
+    b = check_string(B)
+    c = check_string(C)
+
+while True:
+    numbers = input("Please input real numbers for A, B, and C")
+    inputs = numbers.split(",")
+    if (len(inputs) != 3):
+        print("Invalid format")
+    if (values == 'exit'):
+        break
+
+dis = b** - 4*a*c
+if dis < 0:
+    print("There are no real solutions")
+elif dis == 0:
+    x = (-b + dis.sqrt())/2 * a
+    print(f"There is one real solution: x={x}")
+else:
+    y = (-b - dis.sqrt())/2 * a
+    z = (-b + dis.sqrt())/2 * a
+    print(f"There are two real solutions: x={y} and x={z}")
